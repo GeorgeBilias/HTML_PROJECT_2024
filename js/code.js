b9630bb21dcdd4f1fd3beb3de8ee9887bab1e9e8
George Bilias <93704930+GeorgeBilias@users.noreply.github.com>
2024-12-31 20:57:44 +0200

    I need to add comments

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Check if the greeting has already been shown
     if (!sessionStorage.getItem('greetingShown')) {
         const currentDate = new Date();
         const hours = currentDate.getHours();
@@ -16,24 +16,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const welcomeMessage = `${greeting}! Σήμερα είναι ${currentDate.toLocaleDateString()}. Καλώς ήρθατε στον ιστότοπό μας. Εδώ θα βρείτε πληροφορίες για την Hip-Hop κουλτούρα.`;
 
     alert(welcomeMessage);
-    // Set the flag in localStorage to indicate the greeting has been shown
+    
     sessionStorage.setItem('greetingShown', 'true');
 }
 });
-// Function to change the background color of the paragraph
+
 function changeBackgroundColor() {
     const content = document.getElementById('content');
     const paragraph = content.querySelector('p');
-    if (paragraph.style.backgroundColor === "rgb(51, 51, 51)") { // Check if the background color is #333
+    if (paragraph.style.backgroundColor === "rgb(51, 51, 51)") { 
         paragraph.style.backgroundColor = "transparent";
         paragraph.style.color = "initial";
         paragraph.style.padding = "initial";
         paragraph.style.borderRadius = "initial";
     } else {
-        paragraph.style.backgroundColor = "#333"; // Choose the color that fits
-        paragraph.style.color = "#fff"; // Change the text color to white
-        paragraph.style.padding = "10px"; // Add some padding to the paragraph
-        paragraph.style.borderRadius = "5px"; // Add rounded corners
+        paragraph.style.backgroundColor = "#333"; 
+        paragraph.style.color = "#fff"; 
+        paragraph.style.padding = "10px"; 
+        paragraph.style.borderRadius = "5px"; 
     }
 }
 
